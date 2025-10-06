98c08cfecf87f5727d0e2a04d1afc0ee912db8c0
Roman Burlakov <r.burlakov@mail.astondevs.ru>
2025-10-07 01:46:48 +0300

    feat adaptive

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,5 @@
-
+const header = document.querySelector('.header')!;
+const headerButton = header.querySelector('.header__button-burger')!;
+headerButton.addEventListener('click', (event) => {
+  header.classList.toggle('header_open');
+});
