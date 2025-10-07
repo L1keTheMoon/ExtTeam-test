a026f403b467035c4bf046e5c76b01d77f8d6817
Roman Burlakov <r.burlakov@mail.astondevs.ru>
2025-10-08 01:23:32 +0300

    feat slider

---
+++ src/main.js
@@ -0,0 +1,25 @@
+const header = document.querySelector('.header');
+const headerButton = header.querySelector('.header__button-burger');
+headerButton.addEventListener('click', () => {
+  header.classList.toggle('header_open');
+});
+
+new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.slider__button-next',
+    prevEl: '.slider__button-prev',
+  }
+});
+
+const techButtons = document.querySelectorAll('.technologies__button');
+techButtons.forEach(button => {
+  button.addEventListener('click', event => {
+    techButtons.forEach(b => b.classList.remove('technologies__button_active'));
+    event.currentTarget.classList.add('technologies__button_active');
+  })
+});
--- src/main.ts
+++
@@ -1,5 +0,0 @@
-const header = document.querySelector('.header')!;
-const headerButton = header.querySelector('.header__button-burger')!;
-headerButton.addEventListener('click', (event) => {
-  header.classList.toggle('header_open');
-});
