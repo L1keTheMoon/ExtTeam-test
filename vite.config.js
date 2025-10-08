6fd8d42e4e83937de7a6e6c14b9a1cddb8905656
Roman Burlakov <r.burlakov@mail.astondevs.ru>
2025-10-08 16:54:32 +0300

    fix svg issues

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: 'https://l1kethemoon.github.io/ExtTeam-test/',
+  build: {
+    assetsInlineLimit: 0 // отключает инлайнинг всех assets
+  }
 })
